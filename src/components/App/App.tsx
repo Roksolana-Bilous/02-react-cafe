90f152ea3bafba9625ca0dcc29320b2e3b28b0f4
Roksolana <adelaw283@gmail.com>
2025-06-30 00:24:38 +0300

    add app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,59 +1,61 @@
-import css from './App.module.css'
+import css from "./App.module.css";
 
-import CafeInfo from '../CafeInfo/Cafeinfo.tsx'
-import VoteOptions from '../VoteOptions/VoteOptions.tsx'
-import VoteStats from '../VoteStatus/Votestatus.tsx'
-import Notification from '../Notification/Notification'
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 
-import { useState } from 'react'
-import type { Votes } from '../../types/votes.ts'
-import type { VoteType } from '../../types/votes.tsx'
+import Notification from "../Notification/Notification.tsx";
 
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+
+import VoteStatus from "../VoteStatus/VoteStatus.tsx";
+
+import type { Votes, VoteType } from "../../types/votes.ts";
+
+import { useState } from "react";
 
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-    const [votes, setVotes] = useState<Votes>({
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    })
-    
-    const handleVote = (type: VoteType) => {
-        setVotes({
-            ...votes,
-            [type]: votes[type] + 1,
-        })
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
 
-    }
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
 
-    const totalVotes = votes.good + votes.neutral + votes.bad;
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
-    const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
-
-    const resetVotes = () => {
-        setVotes({
-            good: 0,
-            neutral: 0,
-            bad: 0,
-        })
-    }
-
-    const isReset = totalVotes !== 0;
-
-
-    return (
-        
-        <div className={css.app}>
-            <CafeInfo />
-
-            <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={isReset} />
-            
-            {totalVotes > 0 ?
-                (<VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />)
-                : (<Notification />)
-            
-            }
-            
-        </div>
-    )
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0 ? true : false}
+      />
+      {totalVotes ? (
+        <VoteStatus
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
 }
